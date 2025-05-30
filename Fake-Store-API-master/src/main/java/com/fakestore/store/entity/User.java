580ebaad2c2677127300094faac06fd948d1477e
Kamesh-Mishra <kamesh.mishra20@gmail.com>
2025-05-30 14:42:35 +0530

    Added new file

---
+++ Fake-Store-API-master/src/main/java/com/fakestore/store/entity/User.java
@@ -0,0 +1,27 @@
+package com.fakestore.store.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id; 
+   
+	private String username; 
+   
+    private String password;
+}
