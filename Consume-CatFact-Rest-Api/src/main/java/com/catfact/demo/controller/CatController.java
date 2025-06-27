72a83906b426d975e2a4c52dd7a1f5de800a804f
Kamesh-Mishra <kamesh.mishra20@gmail.com>
2025-06-27 13:36:37 +0530

    Added new file

--- Consume-CatFact-Rest-Api/src/main/java/com/catfact/demo/controller/CatController.java
+++ Consume-CatFact-Rest-Api/src/main/java/com/catfact/demo/controller/CatController.java
@@ -3,17 +3,9 @@ package com.catfact.demo.controller;
 
 import lombok.AllArgsConstructor;
 
-import java.util.List;
-
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import com.catfact.demo.entity.CatBreeds;
 import com.catfact.demo.service.CatService;
 
 @AllArgsConstructor
@@ -29,9 +21,4 @@ public class CatController {
 	    }
 	 
 	 
-	 
-	 @GetMapping("/catbreeds")
-	    private List<CatBreeds> getCatBreeds() {
-	       return catService.getCatHBreedAPI(); 
-	    }
 }
--- Consume-CatFact-Rest-Api/src/main/java/com/catfact/demo/service/CatService.java
+++ Consume-CatFact-Rest-Api/src/main/java/com/catfact/demo/service/CatService.java
@@ -1,38 +1,26 @@
 package com.catfact.demo.service;
 
-import java.util.Arrays;
-import java.util.List;
-
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import com.catfact.demo.entity.Cat;
-import com.catfact.demo.entity.CatBreeds;
 
 @Service
 public class CatService {
 	 
 	public String getPublicAPI() {
 	        String url = "https://catfact.ninja/fact";
-	        RestTemplate restTemplate = new RestTemplate();
-	        Cat obj = restTemplate.getForObject(url, Cat.class);
-	        return obj.getFact();
-	     	    }
-	
-	
-	
-	public List<CatBreeds> getCatHBreedAPI() {
-        String url = "https://catfact.ninja/breeds?limit=1000";
-        RestTemplate restTemplate = new RestTemplate();
-//        List<CatBreeds> obj = restTemplate.getForObject(url, List.class);
-//        return obj;
-        
-	    ResponseEntity<CatBreeds[]> response = restTemplate.getForEntity(url,CatBreeds[].class);
-	//	ResponseEntity<Invoice[]> response = restTemplate.exchange(url, HttpMethod.GET, null, Invoice[].class);
-	    CatBreeds[] breeds = response.getBody();
-		List<CatBreeds> list = Arrays.asList(breeds);
+	        
+//			using RestTemplate	        
+//	        RestTemplate restTemplate = new RestTemplate();
+//	        Cat obj = restTemplate.getForObject(url, Cat.class);
+//	        return obj.getFact();
+	        
+//			using WebClient
+	        WebClient.Builder builder = WebClient.builder();
+	        Cat catFact = builder.build().get().uri(url).retrieve().bodyToMono(Cat.class).block();
+	        return catFact.getFact();
 
-		return (list);
-        
-     	    }
+	     	}
+	
+	
 }
