b3dc5b8163bea4f9d74cf5a15bb757850a1bbbb2
Kamesh-Mishra <kamesh.mishra20@gmail.com>
2025-07-01 17:16:20 +0530

    Added new file

---
+++ Spring-Boot-Interceptor-Example/src/main/java/com/interceptor/demo/RequestInterceptorExampleApplication.java
@@ -0,0 +1,13 @@
+package com.interceptor.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestInterceptorExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestInterceptorExampleApplication.class, args);
+	}
+
+}
